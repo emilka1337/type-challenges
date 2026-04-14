6c4cc1a8b86adb508dc683373f841121acb479fa
Emil <Emil.Mahmudov@kapitalbank.az>
2026-04-14 14:41:18 +0400

    00268 #solved

--- playground/easy/00268-easy-if.ts
+++ playground/easy/00268-easy-if.ts
@@ -19,7 +19,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type If<C, T, F> = any
+type If<C extends true | false, T, F> = C extends true ? T : C extends false ? F : never;
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
