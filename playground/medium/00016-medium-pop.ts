daa8b3c1e3d2ef3ae024553e55b3e9a70ce7021b
Emil <Emil.Mahmudov@kapitalbank.az>
2026-04-16 17:28:58 +0400

    00016 #solved

--- playground/medium/00016-medium-pop.ts
+++ playground/medium/00016-medium-pop.ts
@@ -26,7 +26,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type Pop<T extends any[]> = any
+type Pop<T extends any[]> = T extends [...rest: infer U, any] ? U : []
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
