bd457c680ff7d19bf98266ac456fa07a81ba2508
Emil <Emil.Mahmudov@kapitalbank.az>
2026-04-29 14:08:41 +0400

    00612 #skipped

--- playground/medium/00612-medium-kebabcase.ts
+++ playground/medium/00612-medium-kebabcase.ts
@@ -24,7 +24,7 @@
 
 /* _____________ Your Code Here _____________ */
 
-type KebabCase<S> = any
+type KebabCase<S extends string> = any
 
 /* _____________ Test Cases _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
